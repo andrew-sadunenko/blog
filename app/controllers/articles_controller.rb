ec36edad90c9496f4a8b6a09b3323cfe2b595fbf
Andrew <sadunenkoandrew@gmail.com>
2016-12-11 17:53:05 +0200

    add edit article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,17 @@ class ArticlesController < ApplicationController
 		def index
 			@articles = Article.all
 		end
+
 		def new
+			@article = Article.new
+		end
+
+		def show
+			@article = Article.find(params[:id])
+		end
+
+		def edit
+			@article = Article.find(params[:id])
 		end
 
 		def create
@@ -13,8 +23,11 @@ class ArticlesController < ApplicationController
 			redirect_to @article
 		end
 
-		def show
+		def update
 			@article = Article.find(params[:id])
+
+			@article.update(article_params)
+			redirect_to @article
 		end
 
 	private
